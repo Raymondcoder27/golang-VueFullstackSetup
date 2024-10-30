8b9fd77592b9214e9ca47065de5292155cf5a144
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-30 22:21:51 +0300

    fixes frontend bugs

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,16 +1,16 @@
-import type {Themes} from "@/constants"
+import type { Themes } from "@/constants"
 export type TTheme = (typeof Themes)[number]
 
 export interface ApiResponse<T> {
     message: string
     status: number
-    error?:string
-    data?:T
+    error?: string
+    data?: T
 }
 
 export interface ApiErrorResponse {
-    data:object
-    message:string
-    error:string
-    status:number
+    data: object
+    message: string
+    error: string
+    status: number
 }
